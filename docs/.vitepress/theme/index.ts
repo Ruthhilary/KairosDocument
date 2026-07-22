019455091cd7424aaf79df3c8e5658f140f537f3
Ruth Hilary <akaadouruth@gmail.com>
2026-07-22 22:34:16 +0100

    Add mobile dark/light mode toggle button in navbar

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import MobileAppearance from './MobileAppearance.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   Layout() {
-    return h(DefaultTheme.Layout, null, {})
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-before': () => h(MobileAppearance),
+    })
   },
 }
